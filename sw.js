ab1c5e336adc98924eb1cc300b0607cf8f81ad70
ShaheeraSadia <Shaheerasadia@gmail.com>
2026-05-01 22:03:13 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'toolkit-pro-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Install a service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
